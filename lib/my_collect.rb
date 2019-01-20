159fafb131d8951f4cb97253dca3789f4a636988
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-20 02:20:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,11 @@
 
 def my_collect(collection)
- while 
+   new_collection = [] 
+  i = 0 
+  while i < collection.length 
+    new_collection.push(yield(collection[i]))
+    i += 1 
+  end 
+  new_collection
+end 
+ 
