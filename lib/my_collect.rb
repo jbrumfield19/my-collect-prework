6dc58283c059c3ea5efe53edf5fd97e6fe8e4472
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-20 02:22:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 
 def my_collect(collection)
-   new_collection = [] 
+   collectionTwo = [] 
   i = 0 
   while i < collection.length 
-    new_collection.push(yield(collection[i]))
+    collectionTwo.push(yield(collection[i]))
     i += 1 
   end 
-  new_collection
+  collectionTwo
 end 
  
